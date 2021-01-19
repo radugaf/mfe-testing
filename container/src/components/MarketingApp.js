560c77dbac12f37ebde07e911f9a57b51933f26a
Radu Gafita <radugaf@gmail.com>
2021-01-20 01:55:31 +0200

    Created CI

--- container/src/components/MarketingApp.js
+++ container/src/components/MarketingApp.js
@@ -1,12 +1,12 @@
-import { mount } from 'marketing/MarketingApp'
-import React, {useRef, useEffect } from 'react'
+import { mount } from "marketing/MarketingApp";
+import React, { useRef, useEffect } from "react";
 
 export default () => {
-    const ref = useRef(null)
-    
-    useEffect(() => {
-        mount(ref.current)
-    })
+  const ref = useRef(null);
 
-    return <div ref={ref} />
-}
+  useEffect(() => {
+    mount(ref.current);
+  });
+
+  return <div ref={ref} />;
+};
